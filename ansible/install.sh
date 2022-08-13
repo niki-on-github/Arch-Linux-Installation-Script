8f7e58e4c490b8eb3d5a3541a3d52bc84ed14182
arch <arch@local>
2022-08-13 16:36:46 +0200

    add test script

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-if command -v pacman; then
-    pacman -Sy ansible python-pip
-fi
+LBLUE='\033[1;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
+while true; do
+    echo -en "\nEnter user password: " && read -s user_passphrase
+    echo -en "\nVerify password: " && read -s user_passphrase_verify
+
+    if [ "$user_passphrase" == "$user_passphrase_verify" ] && [ -n "$user_passphrase" ]; then
+        echo -e "\n${GREEN}OK ${NC}" && break
+    else
+        echo -e "${RED}\n[ERROR] password does not match! ${NC}"
+    fi
+done
+
+pacman -Sy ansible python-pip
 ansible-galaxy install -r requirements.yml
 
 ansible-playbook -i ./inventory/localhost ./playbooks/install-setup.yml
@@ -12,7 +25,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-ansible-playbook -i ./inventory/localhost -e '{ "ansible_python_interpreter": "/tmp/chroot_wrapper"}' ./playbooks/install-chroot.yml
+ansible-playbook -i ./inventory/localhost -e '{ "ansible_python_interpreter": "/tmp/chroot_wrapper", "user_password": "'$user_passphrase'"}' ./playbooks/install-chroot.yml
 if [ $? -ne 0 ]; then
     echo "Installation failed"
     exit 1
