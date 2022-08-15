487d8afc570736972ed7d7a748e0fad2f0240c44
arch <arch@local>
2022-08-15 18:45:51 +0200

    add option for unencrypted install

--- ansible/install.sh
+++ ansible/install.sh
@@ -9,28 +9,31 @@ mount -o remount,size=2G /run/archiso/cowspace
 pacman -Sy --noconfirm --needed ansible python-pip
 ansible-galaxy install -r requirements.yml
 
+echo "Installation configuration:"
+cat ./playbooks/group_vars/*
+
 while true; do
     echo -en "\nEnter user password: " && read -s user_passphrase
     echo -en "\nVerify password: " && read -s user_passphrase_verify
 
     if [ "$user_passphrase" == "$user_passphrase_verify" ] && [ -n "$user_passphrase" ]; then
-        echo -e "\n${GREEN}OK ${NC}" && break
+        break
     else
-        echo -e "${RED}\n[ERROR] password does not match! ${NC}"
+        echo -e "${RED}\nERROR: password does not match${NC}"
     fi
 done
 
 lsblk
 ansible-playbook -i ./inventory/localhost ./playbooks/install-setup.yml
 if [ $? -ne 0 ]; then
-    echo "Installation failed"
+    echo -e "${RED}ERROR: Installation failed${NC}"
     exit 1
 fi
 
 ansible-playbook -i ./inventory/localhost -e '{ "ansible_python_interpreter": "/tmp/chroot_wrapper", "user_password": "'$user_passphrase'"}' ./playbooks/install-chroot.yml
 if [ $? -ne 0 ]; then
-    echo "Installation failed"
+    echo -e "${RED}ERROR: Installation failed${NC}"
     exit 1
 fi
 
-echo "Installation complete"
+echo -e "${GREEN} OK: Installation complete${NC}"
