77920630bf61682162a5bf69ebee57baaba4f03e
arch <arch@local>
2022-12-03 16:37:15 +0100

    improve local nixos installer

--- ansible/install-nixos-local.sh
+++ ansible/install-nixos-local.sh
@@ -6,9 +6,8 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 
-lsblk
 echo "loading ..."
-nix-shell -p ansible --run "ansible-playbook -i ./inventory/localhost-nixos ./playbooks/install-nixos.yml"
+nix-shell -p ansible --run "lsblk && ansible-playbook -i ./inventory/localhost-nixos ./playbooks/install-nixos.yml"
 if [ $? -ne 0 ]; then
     echo -e "${RED}ERROR: Installation failed${NC}"
     exit 1
